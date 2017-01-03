bc469d04d16bd295d0a3297edaab7134f801bbed
Megan Rabuse <cogglemat@gmail.com>
2017-01-03 15:11:20 -0600

    Complete function _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -96,6 +96,14 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var wrongAnswers = [];
+    var falseTest = function(item) {
+    	return !test(item);
+    }
+
+    wrongAnswers = _.filter(collection, falseTest);
+
+    return wrongAnswers;
   };
 
   // Produce a duplicate-free version of the array.
